a026c9d66bf87d102b09d0c2cf3026a592233fa9
6410110109 <6410110109@psu.ac.th>
2023-06-30 18:26:35 +0700

    update

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 require("dotenv").config();
 
 const people = require("./people.json");
-const Message = require("./modules/send_message");
-const Sticker = require("./modules/send_sticker");
+const lineNotify = require("line-notify-nodejs")(process.env.LINE_TOKEN);
 
 let index = 0;
 
@@ -11,7 +10,13 @@ const send_task = () => {
   const index_new = index % people.length;
 
   const message = `วันนี้ (${date_now}) เป็นเวรประจำวันของ ${people[index_new].name}`;
-  Message(message);
+  lineNotify
+    .notify({
+      message,
+    })
+    .then(() => {
+      console.log("send completed!");
+    });
 };
 
 const loop = setInterval(() => {
@@ -25,42 +30,33 @@ const loop = setInterval(() => {
   }
 }, 1000);
 
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get("/" , (req , res) => {
+app.get("/", (req, res) => {
+  const data = {
+    index,
+    now_people: people[index].name,
+    people,
+  };
 
-    const data = {
-        index,
-        now_people: people[index].name,
-        people,
-    }
+  res.send(data);
+});
 
-    res.send(data)
+app.post("/index", (req, res) => {
+  const index_input = req.body.index;
 
-})
+  index = index_input;
 
+  res.json("OK");
+});
 
-app.post("/index" , (req , res) => {
+app.get("/send", (req, res) => {
+  send_task();
 
-    const index_input = req.body.index
-    
-    index = index_input
+  res.json("OK");
+});
 
-    res.json("OK")
-
-
-})
-
-app.get("/send" , (req , res) => {
-
-    send_task()
-
-    res.json("OK")
-
-
-})
-
-app.listen(1392 , () => console.log("Server is running..."))
+app.listen(1392, () => console.log("Server is running..."));
